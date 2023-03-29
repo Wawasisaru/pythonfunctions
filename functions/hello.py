177e2a85fd1a516e674581a4ddd51c87781ad5ee
Wawasisaru <nsaruwawasi@gmil,.com>
2023-03-29 19:20:32 +0300

    first python

---
+++ functions/hello.py
@@ -0,0 +1,6 @@
+def hello(name):
+    print(f"Hello{name}")
+
+def welcome(student, school):
+    print(f"Welcome to {school},{student}")
+
---
+++ functions/math_1.py
@@ -0,0 +1,19 @@
+def add(a,b):
+    answer = a+b
+    return answer
+
+def sub(c,d):
+    answer = c-d
+    return answer
+
+def multiply(e,f):
+    answer = e*f
+    return answer
+
+def divide(n,m):
+    answer = n/m
+    return answer
+
+def reminder(q,p):
+    answer = q%p
+    return answer
